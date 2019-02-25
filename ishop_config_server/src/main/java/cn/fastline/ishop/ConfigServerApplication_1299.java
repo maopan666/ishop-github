12be36a139b6f72033fe3738d23a755d788612a5
maopan666 <mvp3123@163.com>
2019-02-25 15:05:32 +0800

    第二次

---
+++ ishop_config_server/src/main/java/cn/fastline/ishop/ConfigServerApplication_1299.java
@@ -0,0 +1,15 @@
+package cn.fastline.ishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication_1299 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication_1299.class);
+    }
+}
